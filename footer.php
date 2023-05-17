f3e759fc37edf8b8d4b6db3ee7217228c13c3cc7
Hugo <hugo.hrst3@gmail.com>
2023-05-17 15:06:16 +0200

    hugo-footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <p>IA Pau - Tous droits réservés ©</p>
+    <p>IA Pau - Tous droits réservés ©
     <p>Léo-Paul, Noé, Sacha, Marc-Antoine, Hugo</p>
 </footer>
---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="footer.css" />
+    </head>
+    <body>
+        <footer>
+            <?php
+                include("footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
+
