53cbd86531eb0a41e2a940e58f2cbd3aec88cd1b
Sacha <grumelarts@cy-tech.fr>
2023-05-17 15:23:11 +0200

    header2

--- header.php
+++ header.php
@@ -7,8 +7,13 @@
 
 <div class="banniere">
   <img src="img/iapau_round.png" id="logo"/>
-  <h1>IA PAU</h1>
-  <p>L'intelligence artificielle vue des Pyrénées</p>
+  <div class="text">
+  	<h1>IA PAU</h1>
+  	<p>L'intelligence artificielle vue des Pyrénées</p>
+  </div>
+
+  <a href="connexion.php"> <img id="Client" src="img/Client.png" alt="Client"/> </a>
+
 </div>
 
   <!-- Navbar -->
