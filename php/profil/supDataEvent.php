b5f45e1a578b9d0eabca30dcd9337d3a9d513e05
Bigot Léo Paul <leo.bigot14@gmail.com>
2023-05-31 16:11:15 +0200

    Avancement bisbis profils avec Marc en liveshare

--- php/profil/supDataEvent.php
+++ php/profil/supDataEvent.php
@@ -13,42 +13,25 @@ if ($_SESSION["estConnecte"]) {
 }
 
 
-$titre = valid($_POST["titre"]);
-$debut = valid($_POST["debut"]);
-$fin = valid($_POST["fin"]);
-$description = valid($_POST["description"]);
-$donnees = valid($_POST["donnees"]);
-$consignes = valid($_POST["consignes"]);
-$conseils = valid($_POST["conseils"]);
-$idGestionnaire = valid($_POST["idGestionnaire"]);
 $idDataEvent = $_POST["idDataEvent"];
 
- var_dump($_POST);
 $mysqlClient = connexion($serveur, $bdd, $user, $pass);
 
-if (!empty($titre) and !empty($debut) and !empty($fin) and !empty($description) and !empty($donnees) and !empty($consignes) and !empty($conseils) and !empty($idGestionnaire) and !empty($idDataEvent)){
-    modifDataEvent($mysqlClient,$debut, $fin, $description , $titre ,$donnees, $consignes ,$conseils ,$idGestionnaire , $idDataEvent);
+if (!empty($idDataEvent)){
+    supDataEvent($mysqlClient, $idDataEvent);
 }
 
 header("Location: profil.php");
 
 
 
-function modifDataEvent($mysqlClient, $dateDebut, $dateFin, $descript, $titre , $donnees, $consignes, $conseils, $idGestionnaire, $idDataEvent){
+function supDataEvent($mysqlClient, $idDataEvent){
     try {
         
-        $sqlQuery = 'UPDATE DataEvent SET dateDebut = :dateDebut, dateFin = :dateFin, descript = :descript, titre = :titre, donnees = :donnees, consignes = :consignes, conseils = :conseils, idGestionnaire = :idGestionnaire WHERE idDataEvent = :idDataEvent';
+        $sqlQuery = 'DELETE FROM DataEvent WHERE idDataEvent = :idDataEvent';
 
         $updateDataEvent = $mysqlClient -> prepare($sqlQuery);
         $updateDataEvent ->execute([
-            'dateDebut' => $dateDebut,
-            'dateFin' => $dateFin,
-            'descript' => $descript,
-            'titre' => $titre,
-            'donnees' => $donnees,
-            'consignes' => $consignes,
-            'conseils' => $conseils,
-            'idGestionnaire' => $idGestionnaire,
             'idDataEvent' => $idDataEvent,
         ]);
     } catch (Exception $e) {
